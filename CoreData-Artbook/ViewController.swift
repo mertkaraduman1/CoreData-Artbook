2bad280a8424dc2ef2425a95c048d9f1e4e1d559
mertkaraduman <braumfly@hotmail.com>
2024-07-25 07:55:19 +0300

    Initial Commit

---
+++ CoreData-Artbook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-Artbook
+//
+//  Created by Mert Karaduman on 25.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
